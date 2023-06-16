34197d58d26eb5f95d694ed2025823b6021935e9
ahmedaghadi <ahmedaghadi123@gmail.com>
2023-06-16 18:29:04 +0530

    fixing cors issue in backend

--- backend/next.config.js
+++ backend/next.config.js
@@ -4,11 +4,24 @@ const nextConfig = {};
 module.exports = {
   ...nextConfig,
   //fix error: No 'Access-Control-Allow-Origin' header is present on the requested resource
-  async rewrites() {
+  async headers() {
     return [
       {
-        source: "*",
-        destination: "*",
+        // matching all API routes
+        source: "/*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ],
       },
     ];
   },
